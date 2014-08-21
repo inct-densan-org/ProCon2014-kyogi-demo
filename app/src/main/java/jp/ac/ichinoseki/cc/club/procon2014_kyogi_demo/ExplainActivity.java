1c3d8c2a68853dc53ecb6f3dd02062b39f6c634f
tmp1024 <temporary65536>
2014-08-21 19:07:12 +0900

    ExplainActivityでタイトルバーが表示されないようにした

--- app/src/main/java/jp/ac/ichinoseki/cc/club/procon2014_kyogi_demo/ExplainActivity.java
+++ app/src/main/java/jp/ac/ichinoseki/cc/club/procon2014_kyogi_demo/ExplainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 
 
 public class ExplainActivity extends Activity {
@@ -11,6 +12,7 @@ public class ExplainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_explain);
     }
 
