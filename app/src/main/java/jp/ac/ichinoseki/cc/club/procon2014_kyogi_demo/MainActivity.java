3d04fe411f7f543dbc8e77af0401adea6a337a6c
tmp1024 <temporary65536>
2014-08-21 17:18:14 +0900

    表示テキストをxmlに移動

--- app/src/main/java/jp/ac/ichinoseki/cc/club/procon2014_kyogi_demo/MainActivity.java
+++ app/src/main/java/jp/ac/ichinoseki/cc/club/procon2014_kyogi_demo/MainActivity.java
@@ -16,16 +16,9 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
-
-        final Activity activity = this;
-        TextView appTitle = (TextView)this.findViewById(R.id.app_title);
-
-        appTitle.setText("第25回\n全国高専\nプログラミングコンテスト\n競技部門\nデモアプリ");
-        appTitle.setTextSize(36f);
     }
 
     public void doExplain(View view) {
-
     }
 
     @Override
