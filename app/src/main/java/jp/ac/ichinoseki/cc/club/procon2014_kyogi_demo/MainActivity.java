933a5347cea97b6ea5c5513d12371ad6b80f3732
tmp1024 <temporary65536@gmail.com>
2014-08-24 20:30:51 +0900

    画像を交換する機能を実装 選択コストと交換コストを計算する機能を実装

--- app/src/main/java/jp/ac/ichinoseki/cc/club/procon2014_kyogi_demo/MainActivity.java
+++ app/src/main/java/jp/ac/ichinoseki/cc/club/procon2014_kyogi_demo/MainActivity.java
@@ -1,13 +1,17 @@
 package jp.ac.ichinoseki.cc.club.procon2014_kyogi_demo;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.text.SpannableStringBuilder;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 
@@ -17,11 +21,22 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        WindowManager wm  = (WindowManager)getApplicationContext().getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        Point p = new Point();
+        display.getSize(p);
     }
 
     public void doExplain(View view) {
         final Activity activity = this;
         Intent intent = new Intent(activity, jp.ac.ichinoseki.cc.club.procon2014_kyogi_demo.ExplainActivity.class);
+        activity.startActivity(intent);
+    }
+
+    public void doExperience(View view) {
+        final Activity activity = this;
+        Intent intent = new Intent(activity, DrowPuzzle.class);
         activity.startActivity(intent);
     }
 
